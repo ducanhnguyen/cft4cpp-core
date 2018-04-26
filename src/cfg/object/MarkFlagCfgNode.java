e0915b3ef707ef2158300028a349d3b26e9210ce
ducanhnguyen <anhnd.56@gmail.com>
2018-04-26 13:35:07 +0700

    Update

--- src/cfg/object/MarkFlagCfgNode.java
+++ src/cfg/object/MarkFlagCfgNode.java
@@ -1,45 +1,41 @@
 package cfg.object;
 
-/**
- * �?ại diện cho các câu lệnh đánh dấu đặc biệt trong cfg như: nhóm 3 câu lệnh
- * khởi tạo vòng for; câu lệnh try, catch. exception,...
- */
 public class MarkFlagCfgNode extends CfgNode {
 
-    private boolean shouldInBlock = false;
+	private boolean shouldInBlock = false;
 
-    private boolean shouldInSameLine = shouldInBlock();
+	private boolean shouldInSameLine = shouldInBlock();
 
-    public MarkFlagCfgNode(String content) {
-        super(content);
-    }
+	public MarkFlagCfgNode(String content) {
+		super(content);
+	}
 
-    @Override
-    public boolean isNormalNode() {
-        return true;
-    }
+	@Override
+	public boolean isNormalNode() {
+		return true;
+	}
 
-    @Override
-    public boolean shouldDisplayInCFG() {
-        return true;
-    }
+	@Override
+	public boolean shouldDisplayInCFG() {
+		return true;
+	}
 
-    @Override
-    public boolean shouldInBlock() {
-        return shouldInBlock;
-    }
+	@Override
+	public boolean shouldInBlock() {
+		return shouldInBlock;
+	}
 
-    @Override
-    public boolean shouldDisplayInSameLine() {
-        return shouldInSameLine;
-    }
+	@Override
+	public boolean shouldDisplayInSameLine() {
+		return shouldInSameLine;
+	}
 
-    public void setInBlock(boolean shouldInBlock) {
-        this.shouldInBlock = shouldInBlock;
-    }
+	public void setInBlock(boolean shouldInBlock) {
+		this.shouldInBlock = shouldInBlock;
+	}
 
-    public void setInSameLine(boolean shouldInSameLine) {
-        this.shouldInSameLine = shouldInSameLine;
-    }
+	public void setInSameLine(boolean shouldInSameLine) {
+		this.shouldInSameLine = shouldInSameLine;
+	}
 
 }
